2518d8a4015f1fcac97df5f8aacb038849977099
isaacweathers <isaac@isaacweathers.net>
2025-04-28 19:42:17 +0000

    adding in docs

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+@app.route("/about")
+def about():
+    return app.send_static_file("about.html")
+
+@app.route("/diagrams")
+def diagrams():
+    return app.send_static_file("diagrams.html")
+
+@app.route("/contact")
+def contact():
+    return app.send_static_file("contact.html")
